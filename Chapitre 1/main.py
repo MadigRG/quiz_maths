8010baa9cbd6c1269465cc1a04d07316fc339599
Nolwenn <np.cousin@gmail.com>
2025-07-19 22:38:00 +0200

    Initialisation projet

---
+++ Chapitre 1/main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Quiz Chapitre 1 - Les suites", layout="centered")
+
+st.title("🧠 Chapitre 1 - Révision sur les suites")
+st.write("Ce quiz comporte plusieurs questions. Répondez à toutes les questions, puis validez à la fin pour voir votre score et les explications.")
